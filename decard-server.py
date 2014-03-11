d172287b3d53ac6e6ee24047083151261c0907cd
Stefan Plug <stefanplug@gmail.com>
2014-03-11 12:29:32 +0100

    server now prints a clients message

--- decard-server.py
+++ decard-server.py
@@ -10,117 +10,37 @@ from time import sleep
 from scapy.all import *
 
 def usage():
-	print("Usage: mercurius -S[erver] -s[port_mode] -d[ip_mode] -f[ile] -k[ey]\n"
-		"-S[erver] *Listen for incomming covered messages\n"
-		"-s[port_mode] *Uses TCP source ports as cover channel\n"
-		"	-6 2001::2 *IPv6 Destination\n"
-		"	-4 192.168.1.2 *IPv4 Destination\n"
-		"\n"
-		"-d[ip_mode] *Uses the destination IPv6 address as cover channel\n"
-		"	-n[etwork] 2001:: *The destination IPv6 network\n"
-		"\n"
-		"-f[ile] *File used to store the message to be sent, or recieved\n"
-		"-k[ey] *Key used for en/decryption\n"
+	print("Usage: decard-server -g[roup]\n"
+		"-g[roup] 5 *The group size, default is 5\n"
 	)
 	sys.exit(2)
 
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv, "hSs6:4:dn:f:k:", ["help", 'Server', 'sport_mode', 'ipv6_dst=', 'ipv4_dst=', 'dip_,mode', 'network=', 'file=', 'key='])
+		opts, args = getopt.getopt(argv, "g:", ["help", 'group='])
 	except getopt.GetoptError:
 		usage()
 
 	#defaults
-	server = 0
-	mode = 2
-	network = '2001:610:158:2070:0:2::/96'
-	ipv = 6
-	ipv6_dst = '2001::2'
-	ipv4_dst = '192.168.10.2'
-	key = ':Yjds52%9wnsjp>)'
-	bestand = 'pass'
-	sourceip = '2001:610:158:2070::2'
+	groupsize = 5
 
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
 			usage()
-		elif opt in ("-S", "--Server"):
-			server = 1
-		elif opt in ("-s", "--sport_mode"):
-			if mode == 0:
-				mode = 1
-			else:
-				usage()
-		elif opt in ("-6", "--ipv6_dst"):
-			if mode == 1 and server == 0:
-				ipv6_dst = arg
-				ipv = 6
-			else:
-				usage()
-		elif opt in ("-4", "--ipv4_dst"):
-			if mode == 1 and server == 0:
-				ipv4_dst = arg
-				ipv = 4
-			else:
-				usage()
-		elif opt in ("-d", "--dip_mode"):
-			if mode == 0:
-				mode = 2
-			else:
-				usage()
-		elif opt in ("-n", "--network"):
-			if mode == 2:
-				network = arg
-			else:
-				usage()
-		elif opt in ("-f", "--file"):
-			bestand = arg
-		elif opt in ("-k", "--key"):
-			key = arg
-	if mode == 0:
-		usage()
+		elif opt in ("-g", "--group"):
+			groupsize = arg
 
-	network = network.split('/')
-	netmask = network[1]
-	network = network[0]
+	#start being a deckard server
+	while 1:
+		msg = listen()
+		print msg
 
-	if server == 1:
-		while 1:
-			if mode == 1:
-				msg = recieve_sp()
-			elif mode == 2:
-				msg = recieve_dip6(network, netmask)
-			clear = decrypt(key, msg)
-			if clear != -1:
-				print 'Recieved: '+ clear
-				f = open('pass', 'a')
-				f.write(clear)
-				f.close()
-	else:
-		while 1:
-			f = open(bestand, 'r')
-			for msgid, line in enumerate(f):
-				msg = encrypt(key, line)
-				if mode == 1:
-					send_sp(msg, ipv6_dst)
-				elif mode == 2:
-					send_dip6(msgid, msg, network, sourceip)
-				sleep(1)
-			f.close()
-			sleep(10)
-
-def recieve_sp():
+#Listen for Decard nodes
+def listen():
 	msg = []
 	while 1:
-		recieved = sniff(filter="tcp and port 80", count=1)
-		if hasattr(recieved[0], 'sport'):
-			if recieved[0].sport == 30000:
-				return str(msg)
-			else:
-				try:
-					msg.append(chr(recieved[0].sport - 10000))
-				except ValueError:
-					print 'Strange sport detected: '+ str(recieved[0].sport)
+		recieved = sniff(filter="tcp and port 1337", count=1)
+			return str(msg)
 
 def byte_converter(x, size):
 	teller = len(x) - (len(x) * 2)
