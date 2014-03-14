b5bea46aaa93e1ff55c448c5cc1970c7f338e286
Sean Rijs <Sean.Rijs@os3.nl>
2014-03-14 23:21:55 +0100

    improved logging

--- deckard-client.py
+++ deckard-client.py
@@ -2,9 +2,9 @@
 import ConfigParser
 import sys
 import getopt
-import argparse
 import socket
 import json
+import logging
 
 def usage():
     print("Usage: decard-client -s 127.0.0.1 -p 1337\n\n"
@@ -19,18 +19,27 @@ def client(ip, port, message):
     sock.connect((ip, port))
     sock.sendall(message)
     sock.settimeout(5.0)
-    msg = sock.recv(1024)
+    try:
+        msg = sock.recv(1024)
+    except socket.timeout:
+        logging.error("Connection to server timed out")
+        quit()
     msg_decoded = json.loads(msg)
-    print(msg_decoded)
+    logging.info(msg_decoded)
     for key, value in msg_decoded.iteritems():
-        print key, value
+        logging.info(key, value)
+        if key == "UPDATE":
+            for slave in value:
+                check_node(slave)
+        if key == "ERROR":
+            logging.error("Maybe you are already known.")
     #if "ERROR" in response:
     #    print(response)
     #if "UPDATE" in response:
     #    print(response)
         #    parsed = json.loads(response)
 
-def check_node(ip):
+#def check_node(ip):
 
 
 def main(argv):
@@ -44,8 +53,12 @@ def main(argv):
         usage()
 
     # assign parameters to variables
+    #log = logging
+    #log.basicConfig(format="%(levelname)s: %(message)s")
+    # create logger
+    log = logging
+    loglevel = log.WARNING
     port = 1337
-    verbose = 0
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             usage()
@@ -54,8 +67,9 @@ def main(argv):
         elif opt in ("-p", "--port"):
             port = int(arg)
         elif opt in ("-v", "--verbose"):
-            verbose = 1
+            loglevel=log.DEBUG
     # start connecting
+    log.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=loglevel)
     client(ip, port, "hello")
 
 if __name__ == "__main__":
