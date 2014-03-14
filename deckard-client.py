64772c349eefbc004352492295d95c1ec11c41f3
Sean Rijs <Sean.Rijs@os3.nl>
2014-03-14 20:51:51 +0100

    cleaned up client code

--- deckard-client.py
+++ deckard-client.py
@@ -7,39 +7,39 @@ import socket
 import json
 
 def usage():
-    print("Usage: decard-client -s 127.0.0.1 -p 1337\n"
-        "-s[erver] 127.0.0.1     *The group size, default is 5\n"
-        "-p[ort] 1337            *Verbose mode"
+    print("Usage: decard-client -s 127.0.0.1 -p 1337\n\n"
+        "\t-s[erver] 127.0.0.1     *IP address\n"
+        "\t-p[ort] 1337            *Port number\n"
+        "\t-v                      *Verbose mode\n"
     )
     sys.exit(2)
 
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
-    try:
-        sock.sendall(message)
-        sock.settimeout(5.0)
-        msg = sock.recv(1024)
-        msg_decoded = json.loads(msg)
-        print(msg_decoded)
-        for key, value in msg_decoded.iteritems():
-            print key, value
-        #if "ERROR" in response:
-        #    print(response)
-        #if "UPDATE" in response:
-        #    print(response)
+    sock.sendall(message)
+    sock.settimeout(5.0)
+    msg = sock.recv(1024)
+    msg_decoded = json.loads(msg)
+    print(msg_decoded)
+    for key, value in msg_decoded.iteritems():
+        print key, value
+    #if "ERROR" in response:
+    #    print(response)
+    #if "UPDATE" in response:
+    #    print(response)
         #    parsed = json.loads(response)
-            
-    finally:
-        sock.close()
+
+def check_node(ip):
+
 
 def main(argv):
     # first parameters, then config file, else print help output
     opts, args = getopt.getopt(argv, "s:p:hv", ['server=',
-                                                    'port=',
-                                                    'help',
-                                                    'verbose'
-                                                  ])
+                                                'port=',
+                                                'help',
+                                                'verbose',
+                                               ])
     if opts == []: 
         usage()
 
