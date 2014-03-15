a102200b779179e01ac1f3ff9e82f3320a3104eb
Stefan Plug <stefanplug@gmail.com>
2014-03-15 13:41:28 +0100

    trivial change

--- deckard-server.py
+++ deckard-server.py
@@ -65,8 +65,8 @@ def generate_nodelist(salt, protocol):
     salt = salt.encode('utf-8')
     #create a hashed nodelist and sort the list
     for node in data:
-        #hashed_addr = hashlib.sha1(str(salt) + node[0]).hexdigest()
-        hashed_addr = hashlib.sha1(salt).hexdigest
+        hashed_addr = hashlib.sha1(salt + node[0]).hexdigest()
+        #hashed_addr = hashlib.sha1(salt).hexdigest
         nodelist.append([hashed_addr, str(node[0]), 0])
     nodelist = sorted(nodelist)
     if verbose == 1:
