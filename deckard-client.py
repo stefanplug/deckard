fcdd516e367474ab108fb693e59dd6fbb7ccc52e
Sean Rijs <Sean.Rijs@os3.nl>
2014-03-15 13:10:29 +0100

    parsed json TTL

--- deckard-client.py
+++ deckard-client.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
-import ConfigParser
+#!/usr/bin/python3
+#import ConfigParser
 import sys
 import getopt
 import socket
+import subprocess
 import json
 import logging
 
@@ -14,33 +15,48 @@ def usage():
     )
     sys.exit(2)
 
+def parse_slaves(dct):
+    if 'UPDATE' in dct:
+        return(dct['SLAVES'])
+
+def parse_ttl(dct):
+    if 'TTL' in dct:
+        return(dct['TTL'])
+        
+
 def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    sock.sendall(message)
-    sock.settimeout(5.0)
-    try:
-        msg = sock.recv(1024)
-    except socket.timeout:
-        logging.error("Connection to server timed out")
-        quit()
-    msg_decoded = json.loads(msg)
-    logging.info(msg_decoded)
-    for key, value in msg_decoded.iteritems():
-        logging.info(key, value)
-        if key == "UPDATE":
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    try:
+#        sock.connect((ip, port))
+#    except socket.error as err:
+#        logging.error(err)
+#        sys.exit(2)
+#    sock.sendall(message)
+#    sock.settimeout(5.0)
+#    try:
+#        msg = sock.recv(1024)
+#    except socket.timeout:
+#        logging.error("Connection to server timed out")
+#        return 2
+    # decode the JSON slaves
+    msg_slaves = json.loads('{"UPDATE": true, "SLAVES": ["145.100.108.229", "145.100.108.234", "145.100.108.233", "145.100.108.230"], "TTL": 3600}', object_hook=parse_slaves)
+    logging.info("Slaves: %s", msg_slaves)
+    # decode the JSON TTL
+    msg_ttl = json.loads('{"UPDATE": true, "SLAVES": ["145.100.108.229", "145.100.108.234", "145.100.108.233", "145.100.108.230"], "TTL": 3600}', object_hook=parse_ttl)
+    logging.info("TTL: %s", msg_ttl)
+    #for key, value in msg_decoded.iteritems():
+    for item in msg_decoded:
+        logging.info(item)
+        if key == 'UPDATE':
             for slave in value:
                 check_node(slave)
-        if key == "ERROR":
-            logging.error("Maybe you are already known.")
-    #if "ERROR" in response:
-    #    print(response)
-    #if "UPDATE" in response:
-    #    print(response)
-        #    parsed = json.loads(response)
+        if key == 'TTL':
+            logging.info("Setting TTL to %s", value)
+        if key == 'ERROR':
+            logging.error("Server returned an ERROR message")
 
-#def check_node(ip):
-
+def check_node(ip):
+    subprocess.check_call("ping -c 3 %s" % (IP),shell=True)
 
 def main(argv):
     # first parameters, then config file, else print help output
@@ -53,9 +69,6 @@ def main(argv):
         usage()
 
     # assign parameters to variables
-    #log = logging
-    #log.basicConfig(format="%(levelname)s: %(message)s")
-    # create logger
     log = logging
     loglevel = log.WARNING
     port = 1337
@@ -68,9 +81,12 @@ def main(argv):
             port = int(arg)
         elif opt in ("-v", "--verbose"):
             loglevel=log.DEBUG
-    # start connecting
-    log.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=loglevel)
-    client(ip, port, "hello")
+    # start logging 
+    logformat = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    log.basicConfig(format=logformat, level=loglevel)
+    # start connecting to server
+    while 1:
+        client(ip, port, "hello")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
