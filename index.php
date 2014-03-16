6592248ff59f0a02cea8e097ce093ce5d9083321
Stefan Plug <stefanplug@gmail.com>
2014-03-16 23:51:19 +0100

    update index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ while($servers_row = mysqli_fetch_array($servers))
             echo "<tr><td></td>";
         }
         echo "</td><td><b>Node:</b></td><td><b>" . $nodes_row['hostname'] . "</b></td><td><b>" . $nodes_row['v4'] . "</b></td><td><b>last seen by server " . $uptime . " seconds ago</b></td></tr>";
-        $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v4, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id  AND machinestates.master_id!=" . $servers_row['id'] . " AND machinestates.slave_id=" . $nodes_row['id']);
+        $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v4, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id  AND machinestates.master_id!=" . $servers_row['id'] . " AND machinestates.slave_id=" . $nodes_row['id'] ." AND machinestates.protocol=4");
         while($masters = mysqli_fetch_array($master_nodes))
         {
             $updatetime = time() - $masters['tstamp'];
@@ -48,34 +48,45 @@ while($servers_row = mysqli_fetch_array($servers))
     }
 }
 
-/* create the ring tables
-echo "<h3>Ring tables<h></h></h3><p>The following tables show what nodes were seen by what other nodes</p>";
-$slave_nodes = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v4 IS NOT NULL)");
-while($slaves = mysqli_fetch_array($slave_nodes))
+// create a list of which node have been seen by the server
+echo "<h2>IPv6</h>";
+$servers = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE deckardserver=1 AND v6 IS NOT NULL");
+while($servers_row = mysqli_fetch_array($servers))
 {
-    echo "<table border='1'><tr><td><span style='font-weight:bold'>Slave node:</span></td><td><span style='font-weight:bold'>" . $slaves['hostname'] . "</span></td><td><span style='font-weight:bold'>" . $slaves['v4'] . "</span></td></tr>";
-    $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v4, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id AND machinestates.slave_id=" . $slaves['id']);
-
-    while($masters = mysqli_fetch_array($master_nodes))
+    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row['hostname'] . "</b></td><td><b>" . $servers_row['v6'] . "</b></td></tr>";
+    $nodes = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v6 IS NOT NULL)");
+    while($nodes_row = mysqli_fetch_array($nodes))
     {
-        $updatetime = time() - $masters['tstamp'];
-        if($updatetime < $staleout_time)
+        $server_seen = mysqli_query($con,"SELECT machinestates.tstamp, machinestates.active FROM machines, machinestates WHERE machines.id = machinestates.slave_id AND machinestates.slave_id=" . $nodes_row['id'] ." AND machinestates.master_id=" . $servers_row['id']);
+        $updatetime = mysqli_fetch_array($server_seen);
+        $uptime = time() - $updatetime['tstamp'];  
+        if($uptime > $staleout_time)
         {
-            echo "<tr><td>Master node:</td><td>" . $masters['hostname'] . "</td><td>" . $masters['v4'] . "</td>";
-            if($masters['active'] == 1)
-            {
-                echo "<td bgcolor='green'>up</td>";
-            }
-            else
-            {
-                echo "<td bgcolor='red'>down</td>";
-            }
-            echo "<td>" . $updatetime . " seconds ago</tr>";
+            echo "<tr bgcolor=red><td><b>*</b></td>";
         }
-        echo "</table>";
+        else
+        {
+            echo "<tr><td></td>";
+        }
+        echo "</td><td><b>Node:</b></td><td><b>" . $nodes_row['hostname'] . "</b></td><td><b>" . $nodes_row['v6'] . "</b></td><td><b>last seen by server " . $uptime . " seconds ago</b></td></tr>";
+        $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v6, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id  AND machinestates.master_id!=" . $servers_row['id'] . " AND machinestates.slave_id=" . $nodes_row['id']." AND machinestates.protocol=41");
+        while($masters = mysqli_fetch_array($master_nodes))
+        {
+            $updatetime = time() - $masters['tstamp'];
+            if($updatetime < $staleout_time)
+            {
+                if($masters['active'] == 1)
+                {
+                    echo "<tr><td></td><td></td><td>" . $masters['hostname'] . "</td><td>" . $masters['v6'] . "</td><td>UP " . $updatetime . " seconds ago</td></tr>";
+                }
+                else
+                {
+                    echo "<tr bgcolor='red'><td>*</td><td></td><td>" . $masters['hostname'] . "</td><td>" . $masters['v6'] . "</td><td>DOWN " . $updatetime . " seconds ago</td></tr>";
+                }
+            }
+        }
     }
 }
-*/
 echo "</table></body></html>";
 
 mysqli_close($con);
