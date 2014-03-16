dfc098d27b29a41a6106292d3502e4ca11abd602
Stefan Plug <stefanplug@gmail.com>
2014-03-17 00:04:01 +0100

    update index

--- index.php
+++ index.php
@@ -50,17 +50,17 @@ while($servers_row = mysqli_fetch_array($servers))
 
 // create a list of which node have been seen by the server
 echo "<br /><br /><h2>IPv6</h>";
-$servers = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE deckardserver=1 AND v6 IS NOT NULL");
-while($servers_row = mysqli_fetch_array($servers))
+$servers6 = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE deckardserver=1 AND v6 IS NOT NULL");
+while($servers_row6 = mysqli_fetch_array($servers6))
 {
-    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row['hostname'] . "</b></td><td><b>" . $servers_row['v6'] . "</b></td></tr>";
-    $nodes = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v6 IS NOT NULL)");
-    while($nodes_row = mysqli_fetch_array($nodes))
+    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row6['hostname'] . "</b></td><td><b>" . $servers_row6['v6'] . "</b></td></tr>";
+    $nodes6 = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v6 IS NOT NULL)");
+    while($nodes_row6 = mysqli_fetch_array($nodes6))
     {
-        $server_seen = mysqli_query($con,"SELECT machinestates.tstamp, machinestates.active FROM machines, machinestates WHERE machines.id = machinestates.slave_id AND machinestates.slave_id=" . $nodes_row['id'] ." AND machinestates.master_id=" . $servers_row['id']);
-        $updatetime = mysqli_fetch_array($server_seen);
-        $uptime = time() - $updatetime['tstamp'];  
-        if($uptime > $staleout_time)
+        $server_seen6 = mysqli_query($con,"SELECT machinestates.tstamp, machinestates.active FROM machines, machinestates WHERE machines.id = machinestates.slave_id AND machinestates.slave_id=" . $nodes_row6['id'] ." AND machinestates.master_id=" . $servers_row6['id']);
+        $updatetime6 = mysqli_fetch_array($server_seen6);
+        $uptime6 = time() - $updatetime6['tstamp'];  
+        if($uptime6 > $staleout_time)
         {
             echo "<tr bgcolor=red><td><b>*</b></td>";
         }
@@ -68,20 +68,20 @@ while($servers_row = mysqli_fetch_array($servers))
         {
             echo "<tr><td></td>";
         }
-        echo "</td><td><b>Node:</b></td><td><b>" . $nodes_row['hostname'] . "</b></td><td><b>" . $nodes_row['v6'] . "</b></td><td><b>last seen by server " . $uptime . " seconds ago</b></td></tr>";
-        $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v6, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id  AND machinestates.master_id!=" . $servers_row['id'] . " AND machinestates.slave_id=" . $nodes_row['id']." AND machinestates.protocol=41");
-        while($masters = mysqli_fetch_array($master_nodes))
+        echo "</td><td><b>Node:</b></td><td><b>" . $nodes_row6['hostname'] . "</b></td><td><b>" . $nodes_row6['v6'] . "</b></td><td><b>last seen by server " . $uptime6 . " seconds ago</b></td></tr>";
+        $master_nodes6 = mysqli_query($con,"SELECT machines.hostname, machines.v6, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id AND machinestates.master_id!=" . $servers_row6['id'] . " AND machinestates.slave_id=" . $nodes_row6['id']." AND machinestates.protocol=41");
+        while($masters6 = mysqli_fetch_array($master_nodes6))
         {
-            $updatetime = time() - $masters['tstamp'];
-            if($updatetime < $staleout_time)
+            $updatetime6 = time() - $masters6['tstamp'];
+            if($updatetime6 < $staleout_time)
             {
-                if($masters['active'] == 1)
+                if($masters6['active'] == 1)
                 {
-                    echo "<tr><td></td><td></td><td>" . $masters['hostname'] . "</td><td>" . $masters['v6'] . "</td><td>UP " . $updatetime . " seconds ago</td></tr>";
+                    echo "<tr><td></td><td></td><td>" . $masters6['hostname'] . "</td><td>" . $masters6['v6'] . "</td><td>UP " . $updatetime6 . " seconds ago</td></tr>";
                 }
                 else
                 {
-                    echo "<tr bgcolor='red'><td>*</td><td></td><td>" . $masters['hostname'] . "</td><td>" . $masters['v6'] . "</td><td>DOWN " . $updatetime . " seconds ago</td></tr>";
+                    echo "<tr bgcolor='red'><td>*</td><td></td><td>" . $masters6['hostname'] . "</td><td>" . $masters6['v6'] . "</td><td>DOWN " . $updatetime6 . " seconds ago</td></tr>";
                 }
             }
         }
