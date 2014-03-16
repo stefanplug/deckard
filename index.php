a0b2003587e6806308ad68bfc7383810a1f70bea
Stefan Plug <stefanplug@gmail.com>
2014-03-16 23:20:03 +0100

    update index

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ echo "<html><body><h1>Deckard ring</h1><p>Staleout time: " . $staleout_time . " 
 $servers = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE deckardserver=1 AND v4 IS NOT NULL");
 while($servers_row = mysqli_fetch_array($servers))
 {
-    echo "<table border=0><tr><td></td><td><span style='font-weight:bold'>Server:</span></td><td><span style='font-weight:bold'>" . $servers_row['hostname'] . "</span></td><td><span style='font-weight:bold'>" . $servers_row['v4'] . "</span></td></tr>";
+    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row['hostname'] . "</b></td><td><b>" . $servers_row['v4'] . "</b></td></tr>";
     $nodes = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v4 IS NOT NULL)");
     while($nodes_row = mysqli_fetch_array($nodes))
     {
@@ -22,13 +22,13 @@ while($servers_row = mysqli_fetch_array($servers))
         $uptime = time() - $updatetime['tstamp'];  
         if($uptime > $staleout_time)
         {
-            echo "<tr bgcolor=red><td>*</td>";
+            echo "<tr bgcolor=red><td><b>*</b>b></td>";
         }
         else
         {
             echo "<tr><td></td>";
         }
-        echo "</td><td>Node:</td><td>" . $nodes_row['hostname'] . "</td><td>" . $nodes_row['v4'] . "</td><td>last seen by server " . $uptime . " seconds ago</td></tr>";
+        echo "</td><td><b>Node:</b></td><td><b>" . $nodes_row['hostname'] . "</b></td><td><b>" . $nodes_row['v4'] . "</b></td><td><b>last seen by server " . $uptime . " seconds ago</b></td></tr>";
         $master_nodes = mysqli_query($con,"SELECT machines.hostname, machines.v4, machinestates.active, machinestates.tstamp FROM machines, machinestates WHERE machinestates.master_id=machines.id AND machinestates.slave_id=" . $nodes_row['id']);
         while($masters = mysqli_fetch_array($master_nodes))
         {
@@ -37,11 +37,11 @@ while($servers_row = mysqli_fetch_array($servers))
             {
                 if($masters['active'] == 1)
                 {
-                    echo "<tr bgcolor='green'><td></td><td>Master node:</td><td>" . $masters['hostname'] . "</td><td>" . $masters['v4'] . "</td><td bgcolor='green'>UP " . $updatetime . " seconds ago</td></tr>";
+                    echo "<tr><td></td><td></td><td>" . $masters['hostname'] . "</td><td>" . $masters['v4'] . "</td><td bgcolor='green'>UP " . $updatetime . " seconds ago</td></tr>";
                 }
                 else
                 {
-                    echo "<tr bgcolor='red'><td>Master node:</td><td>" . $masters['hostname'] . "</td><td>" . $masters['v4'] . "</td><td>DOWN " . $updatetime . " seconds ago</td></tr>";
+                    echo "<tr bgcolor='red'><td>*</td><td>Master node:</td><td>" . $masters['hostname'] . "</td><td>" . $masters['v4'] . "</td><td>DOWN " . $updatetime . " seconds ago</td></tr>";
                 }
             }
         }
