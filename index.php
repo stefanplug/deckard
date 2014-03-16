1f88c1142fa8ab6806aa9704029838e2bb444e49
Stefan Plug <stefanplug@gmail.com>
2014-03-16 21:55:39 +0100

    update index

--- index.php
+++ index.php
@@ -17,11 +17,11 @@ echo "<p>The following table shows what nodes were seen by the server</p>";
 $servers = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE deckardserver=1 AND v4 IS NOT NULL");
 while($servers_row = mysqli_fetch_array($servers))
 {
-    echo "<table border=1><th>Table title</th><tr><td><span style='font-weight:bold'>Server:</span></td><td><span style='font-weight:bold'>" . $servers_row[hostname] . "</span></td><td></td><td><span style='font-weight:bold'>" . $row[v4] . "</span></td></tr>"
+    echo "<table border=1><th>Table title</th><tr><td><span style='font-weight:bold'>Server:</span></td><td><span style='font-weight:bold'>" . $servers_row[hostname] . "</span></td><td></td><td><span style='font-weight:bold'>" . $row[v4] . "</span></td></tr>";
     $nodes = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v4 IS NOT NULL)");
     while($nodes_row = mysqli_fetch_array($nodes))
     {
-        echo "<tr><td>Node:</td><td>" . $nodes_row[hostname] . "</td><td>" . $nodes_row[v4] . "</td></tr>"
+        echo "<tr><td>Node:</td><td>" . $nodes_row[hostname] . "</td><td>" . $nodes_row[v4] . "</td></tr>";
     }
 }
 
