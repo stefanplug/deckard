9a1dd52c05087d29a8c1a892eb560f1e6c834388
Stefan Plug <stefanplug@gmail.com>
2014-03-17 00:20:43 +0100

    update index

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ if (mysqli_connect_errno())
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-echo "<html><body><h1>Deckard ring</h1><p>Staleout time: " . $staleout_time . " seconds</p>";
-echo "<h2>IPv4</h>";
-echo "<div>";
+echo "<html><body><h2>Deckard ring</h><p>Staleout time: " . $staleout_time . " seconds</p>";
+echo "<p>IPv4</p>";
+
 // create a list of which node have been seen by the server
 $servers = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE deckardserver=1 AND v4 IS NOT NULL");
 while($servers_row = mysqli_fetch_array($servers))
 {
-    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row['hostname'] . "</b></td><td><b>" . $servers_row['v4'] . "</b></td></tr>";
+    echo "<table><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row['hostname'] . "</b></td><td><b>" . $servers_row['v4'] . "</b></td></tr>";
     $nodes = mysqli_query($con,"SELECT id, hostname, v4 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v4 IS NOT NULL)");
     while($nodes_row = mysqli_fetch_array($nodes))
     {
@@ -49,13 +49,13 @@ while($servers_row = mysqli_fetch_array($servers))
         }
     }
 }
-echo "</div>";
+
 // create a list of which node have been seen by the server
-echo "<h2>IPv6</h>";
+echo "<p>IPv6</p>";
 $servers6 = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE deckardserver=1 AND v6 IS NOT NULL");
 while($servers_row6 = mysqli_fetch_array($servers6))
 {
-    echo "<table border=0><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row6['hostname'] . "</b></td><td><b>" . $servers_row6['v6'] . "</b></td></tr>";
+    echo "<table><tr><td></td><td><b>Server:</b></td><td><b>" . $servers_row6['hostname'] . "</b></td><td><b>" . $servers_row6['v6'] . "</b></td></tr>";
     $nodes6 = mysqli_query($con,"SELECT id, hostname, v6 FROM machines WHERE (deckardserver = 0 OR deckardserver IS NULL) AND (v6 IS NOT NULL)");
     while($nodes_row6 = mysqli_fetch_array($nodes6))
     {
